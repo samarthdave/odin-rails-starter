ea9e2972a8cf8d853b2adb6a933c96e610164d6c
Samarth Dave <4997589+samarthdave@users.noreply.github.com>
2024-06-01 18:41:30 +0000

    Article model layer validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
+# Active Record automatically defines model attributes for every table column, so you don't have to declare those attributes in your model file.
+# so "title" and "body" are immediately available to us
+
 class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
 end
