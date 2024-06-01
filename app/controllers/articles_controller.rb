b29c42dc421dc6f53378214db16709be7e419352
Samarth Dave <4997589+samarthdave@users.noreply.github.com>
2024-06-01 20:45:52 +0000

    edit & update action in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # updates an article (from edit page) with required params
+  #
+  # @param id [number] the identifier of the target article
+  # @param title [String] new title user wants to set
+  # @param body [String] new body users wants to use
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
